34932e43da3bd1ddfd0aa56c4949681bc1ab914c
Gino Farina <farina.gino.94@gmail.com>
2023-02-14 21:09:00 -0300

    tercera clase (14/2/2023): desafio

--- tercera-clase/App.js
+++ tercera-clase/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Item de lista" style={styles.input} />
+          <Button title="Agregar" />
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>Item 1</Text>
+          <Text style={styles.item}>Item 2</Text>
+          <Text style={styles.item}>Item 3</Text>
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30,
   },
+  inputContainer: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  itemContainer: {
+    marginTop: 30,
+  },
+  item: {
+    padding: 10,
+    textAlign: "center",
+  }
 });
