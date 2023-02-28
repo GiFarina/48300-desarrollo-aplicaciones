8a4f58b480091fcbee90c4baed095c91d91cfe74
Gino Farina <ginofarina@192.168.1.2>
2023-02-28 20:13:21 -0300

    sexta clase (28/02/2023): codigo de ejemplo

--- 6-sexta-clase/App.js
+++ 6-sexta-clase/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './src/components/Header';
+import StartGameScreen from './src/screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Adivina el numero" />
+      <StartGameScreen />
     </View>
   );
 }
@@ -13,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
