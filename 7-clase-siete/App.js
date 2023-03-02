d03d3a8a3fe9ce7c3ffec2fca285c27946bbce67
Gino Farina <farina.gino.94@gmail.com>
2023-03-02 19:52:55 -0300

    clase siete (02/03/2023): switch entre pantallas

--- 7-clase-siete/App.js
+++ 7-clase-siete/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './src/components/Header';
 import StartGameScreen from './src/screens/StartGameScreen';
+import GameScreen from './src/screens/GameScreen';
 
 export default function App() {
+
+  const [userNumber, setUserNumber] = React.useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Adivina el numero" />
-      <StartGameScreen />
+      {
+        !userNumber 
+        ? <StartGameScreen onStartGame={startGameHandler} />
+        : <GameScreen userOption={userNumber} /> 
+      }
     </View>
   );
 }
