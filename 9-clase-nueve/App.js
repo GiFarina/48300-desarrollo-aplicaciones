711094aaec8cdee34a685f28a899036a558146fe
Gino Farina <farina.gino.94@gmail.com>
2023-03-09 19:43:13 -0300

    remove styles from app.js

--- 9-clase-nueve/App.js
+++ 9-clase-nueve/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import {useFonts, OpenSans_400Regular, OpenSans_700Bold} from '@expo-google-fonts/open-sans';
 
@@ -28,12 +27,3 @@ export default function App() {
     <ShopNavigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
